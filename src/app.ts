dc623ba6d265a28d02744627d5725013c7791e0d
Farshid Ahsan <arnob.farshid@gmail.com>
2023-10-20 00:36:46 +0600

    added

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://service-booking-management.vercel.app/',
     credentials: true,
   })
 );
